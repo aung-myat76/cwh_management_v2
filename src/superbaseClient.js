57fc5121aff766b0d4d2d6d50a821edc44b1a85a
aung_myat <gusion762389@gmail.com>
2026-08-28 18:02:09 +0630

    add deno url

--- src/superbaseClient.js
+++ src/superbaseClient.js
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // const supabaseUrl = "https://llcbrnpthatrqpltnqwt.supabase.co";
-const workerUrl = "https://cwh-management.kaung762389.workers.dev";
+// const workerUrl = "https://cwh-management.kaung762389.workers.dev";
+const denoUrl = "https://sad-snail-3874.aung-myat76.deno.net";
 // const vercelUrl = "https://cwh-management-v2.vercel.app/supabase-api";
 const supabaseAnonKey = "sb_publishable__rdCpUrZUPQLm6CJ1femZQ_g-4Sx0KC";
 
-export const supabase = createClient(workerUrl, supabaseAnonKey);
+export const supabase = createClient(denoUrl, supabaseAnonKey);
