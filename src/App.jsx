db270c3ce2088c0ca3b0be2dd5fb1271809be3c3
aung_myat <gusion762389@gmail.com>
2026-09-14 22:14:44 +0630

    remove hash

--- src/App.jsx
+++ src/App.jsx
@@ -714,60 +714,58 @@ const App = () => {
     };
 
     return (
-        <Router>
-            <Routes>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <MainLayout
+                        // handleReset={handleReset}
+                        getLastUpdatedTime={getLastUpdatedTime}
+                    />
+                }>
                 <Route
-                    path="/"
+                    index
                     element={
-                        <MainLayout
-                            // handleReset={handleReset}
-                            getLastUpdatedTime={getLastUpdatedTime}
+                        <Loading
+                            trucks={trucks}
+                            loading={loading}
+                            updateLog={updateLog}
+                            updateTruck={updateTruck}
+                            updateCondition={updateCondition}
                         />
-                    }>
-                    <Route
-                        index
-                        element={
-                            <Loading
-                                trucks={trucks}
-                                loading={loading}
-                                updateLog={updateLog}
-                                updateTruck={updateTruck}
-                                updateCondition={updateCondition}
-                            />
-                        }
-                    />
-                    <Route
-                        path="/loading-log"
-                        element={
-                            <LoadingLog
-                                logs={logs}
-                                trucks={trucks}
-                                // setLoadingLogs={setLoadingLogs}
-                                updateLog={updateLog}
-                                deleteLog={deleteLog}
-                            />
-                        }
-                    />
-                    <Route
-                        path="/packaging"
-                        element={
-                            <Packaging lines={lines} updateLine={updateLine} />
-                        }
-                    />
-                    <Route
-                        path="/setting"
-                        element={
-                            <Setting
-                                resetLoadingLogsByDate={resetLoadingLogsByDate}
-                                resetLoadingBay={resetLoadingBay}
-                            />
-                        }
-                    />
-                    <Route path="*" element={<Navigate to={"/"} />} />
-                    {/* {trucks.length > 0 && <Packaging />} */}
-                </Route>
-            </Routes>
-        </Router>
+                    }
+                />
+                <Route
+                    path="/loading-log"
+                    element={
+                        <LoadingLog
+                            logs={logs}
+                            trucks={trucks}
+                            // setLoadingLogs={setLoadingLogs}
+                            updateLog={updateLog}
+                            deleteLog={deleteLog}
+                        />
+                    }
+                />
+                <Route
+                    path="/packaging"
+                    element={
+                        <Packaging lines={lines} updateLine={updateLine} />
+                    }
+                />
+                <Route
+                    path="/setting"
+                    element={
+                        <Setting
+                            resetLoadingLogsByDate={resetLoadingLogsByDate}
+                            resetLoadingBay={resetLoadingBay}
+                        />
+                    }
+                />
+                <Route path="*" element={<Navigate to={"/"} />} />
+                {/* {trucks.length > 0 && <Packaging />} */}
+            </Route>
+        </Routes>
     );
 };
 
